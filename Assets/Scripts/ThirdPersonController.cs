c010530ca545aec570651c2886d0b007e9662dc2
Will Kibel <56790114+willkbl@users.noreply.github.com>
2024-11-21 00:24:52 -0500

    player w/ tank controls and animation switching

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -9,10 +9,11 @@ public class ThirdPersonController : MonoBehaviour
     Animator animator;
     Vector3 input, moveDirection;
 
-    public float moveSpeed = 10;
-    public float jumpHeight = 10;
+    public float moveSpeed = 5f;
+    //public float jumpHeight = 10;
     public float gravity = 9.81f;
-    public float airControl = 10f;
+    //public float airControl = 10f;
+    public float turnSpeed = 180f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,45 +25,15 @@ public class ThirdPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //moveDirection.y -= gravity * Time.deltaTime;
 
-        input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
-
-        input *= moveSpeed;
-
-        if (controller.isGrounded)
-        {
-            moveDirection = input;
-
-            //we can jump
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
-            }
-            else
-            {
-                moveDirection.y = 0.0f;
-            }
-
-        }
-        else
-        {
-            input.y = moveDirection.y;
-            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
-        }
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+        moveDirection = transform.forward * Input.GetAxis("Vertical") * moveSpeed;
 
         moveDirection.y -= gravity * Time.deltaTime;
 
-        controller.Move(moveDirection * Time.deltaTime);
 
-        //rotate the character in camera direction when walking
-        //if (input.magnitude > 0.01f)
-        //{
-        //    float cameraYawRotation = Camera.main.transform.eulerAngles.y;
-        //    Quaternion newRotation = Quaternion.Euler(0f, cameraYawRotation, 0f);
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * 10);
-        //}
+        controller.Move(moveDirection * Time.deltaTime - Vector3.up * 0.1f);
 
 
 
