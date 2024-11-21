f10be9d7b72aec50a7cf7c9e2069dab1245e170d
Will Kibel <56790114+willkbl@users.noreply.github.com>
2024-11-21 01:14:27 -0500

    figured out post processing

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+
+    public Transform player;
+
+    public CinemachineVirtualCamera activeCam;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            activeCam.Priority = 1;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            activeCam.Priority = 0;
+        }
+    }
+}
