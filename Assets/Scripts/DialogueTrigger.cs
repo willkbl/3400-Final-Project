6a955e7afd0a2cb11708e084ab16a6bb78249706
Will Kibel <56790114+willkbl@users.noreply.github.com>
2024-11-26 16:38:14 -0500

    fiddling around with cameras

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,16 +11,22 @@ public class DialogueTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Script runs");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("S key pressed");
+        }
     }
 
     void OnMouseDown(){
         // this object was clicked - do something
+        canvas.gameObject.SetActive(true);
+        Debug.Log("Clicked");
     }
+ 
 }
