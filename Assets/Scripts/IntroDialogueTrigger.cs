decde9767d25b9859643da37483695379c758ca3
Will Kibel <56790114+willkbl@users.noreply.github.com>
2024-12-04 00:39:36 -0500

    lots of miscellaneous things

---
+++ Assets/Scripts/IntroDialogueTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDialogueTrigger : MonoBehaviour
+{
+    public Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
