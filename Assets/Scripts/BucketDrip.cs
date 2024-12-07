c6af8fb8906908b179622c071a955c94e19fe4e1
Will Kibel <56790114+willkbl@users.noreply.github.com>
2024-12-07 15:08:06 -0500

    implemented water bucket

---
+++ Assets/Scripts/BucketDrip.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BucketDrip : MonoBehaviour
+{
+
+    AudioSource dropletSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropletSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Collision!");
+        if (other.CompareTag("WaterDroplet"))
+        {
+            dropletSound.pitch = UnityEngine.Random.Range(0.9f, 1.1f);
+            dropletSound.Play();
+        }
+    }
+}
